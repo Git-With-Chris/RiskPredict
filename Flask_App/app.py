4fd91c795030018adf38a3623d0b176b69d4b5c9
Samuel Mainwood <sammainwood@hotmail.com>
2023-09-18 17:07:02 +1000

    Sam M: Created a rough template function for the multi-graph output. Need to fix input function and ability to display on index.html

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
 from keras.models import load_model
 
 app = Flask(__name__)
 
 # Load Customer Data and Risk Prediction Model
-data = pd.read_csv("Customer_DB.csv")
-Risk_Prediction_Model = load_model('model/Risk_Prediction_Model.h5')
+data = pd.read_csv("Flask_App/Customer_DB.csv")
+Risk_Prediction_Model = load_model('Flask_App/model/Risk_Prediction_Model.h5')
+graphing_df = pd.read_csv('Flask_App/Graph_CSV.csv')
 
 def predict_risk(input_data):
     # Extract the features (excluding the 'ID' column)
@@ -25,13 +28,13 @@ def predict_risk(input_data):
 
     # Make a binary prediction based on the threshold
     if predicted_risk >= threshold:
-        prediction = "Risk"
+        prediction = "High-Risk"
     else:
-        prediction = "Non-Risk"
+        prediction = "Low-Risk"
     
     return predicted_risk, prediction
 
-
+# Get the customer prediction information
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -54,5 +57,64 @@ def index():
 
     return render_template("index.html")
 
+"""
+#######
+This code was causing the website to crash for me
+######
+@app.route("/get_plot", methods = ['GET', 'POST'])
+def graph_generator():
+    if request.method == "POST":
+        cust_id = request.form['cust_id'] # input has not been fixed, I was first trying to get the image to show on the index file
+        sns.boxplot(data = graphing_df, y= "ExternalRiskEstimate", x = "Category")
+        plt.title('Test_graph')
+        plt.savefig('static/my_plot.png') # save in static for referencing
+        return render_template("index.html", plot_url = 'static/my_plot.png') # URL to reference for /get_plot
+    else:
+        return render_template('index.html')
+
+        
+        """
+
+
+"""
+#####
+Rough Start for a function that can return graph via ID
+
+import seaborn as sns
+def graph_generator(customer_id, graph_type, variable):
+    id = customer_id
+    new_df = df
+    new_df['Category'][new_df['ID'] == id] = 1.5
+    if graph_type == "BoxPlot":
+        ax1 = plt.subplot()
+        ax1 = sns.boxplot(data = new_df, x = 'Category', y = variable)
+        ax1.set_xticklabels(['Low Risk', 'High Risk', 'You'])
+        plt.xlabel('Category')
+        plt.ylabel('Normalised External Risk Estimate')
+        plt.title('Model Comparison of External Risk Estimate')
+        plt.show()
+        plt.clf()
+    
+        
+    elif graph_type == "Distribution Plot":
+        x_val = new_df[variable][new_df['ID'] == id]
+        ax1 = plt.subplot()
+        ax1 = sns.kdeplot(data=new_df, x=variable, hue='Category', fill=True)
+    
+        plt.vlines(x=x_val, ymin=0, ymax=2, label="You", colors='red', linestyle='dashed')  # Adjust linestyle and color as needed
+        plt.xlabel('Normalized External Risk Estimate')
+        plt.ylabel('Distribution')
+        plt.title('Model Comparison of External Risk Estimate')
+    
+        # Set the legend labels using plt.legend
+        plt.legend(title='Risk Category', labels=["Low Risk", "High Risk", "You"])
+    
+        plt.show()
+        plt.clf()
+
+        # Might need to savefig in /static for Flask to be able to send it to webpage
+
+"""
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run('127.0.0.1', 5000, debug=True)
